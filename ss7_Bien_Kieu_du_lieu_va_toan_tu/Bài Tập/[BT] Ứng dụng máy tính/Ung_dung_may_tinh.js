abf4a02deb3697894bb70dad5575698c44407387
Thanhtruong2002 <huynhthihoangthu2810@gmail.com>
2022-04-19 21:36:18 +0700

    [BT] Ung_dung_may_tinh

---
+++ ss7_Bien_Kieu_du_lieu_va_toan_tu/Bài Tập/[BT] Ứng dụng máy tính/Ung_dung_may_tinh.js
@@ -0,0 +1,12 @@
+function cong() {
+    document.write(parseInt(document.getElementById("dauvao1").value)+parseInt(document.getElementById("dauvao2").value) )
+}
+function tru() {
+    document.write(parseInt(document.getElementById("dauvao1").value)-parseInt(document.getElementById("dauvao2").value))
+}
+function nhan() {
+    document.write(parseInt(document.getElementById("dauvao1").value)*parseInt(document.getElementById("dauvao2").value))
+}
+function chia() {
+    document.write(parseInt(document.getElementById("dauvao1").value)/parseInt(document.getElementById("dauvao2").value))
+}
